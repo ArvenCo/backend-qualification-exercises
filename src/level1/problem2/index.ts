536458c533baeb6d807f86e65a0dbe918bbbdd00
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2025-01-04 22:11:14 +0800

    solved

--- src/level1/problem2/index.ts
+++ src/level1/problem2/index.ts
@@ -1,5 +1,7 @@
 export class ObjectId {
   private data: Buffer;
+  private static random = Math.floor(Math.random() * 0x7fffffff);
+  private static counter = Math.floor(Math.random() * 0xffffff);
 
   constructor(type: number, timestamp: number) {
     /**
@@ -11,18 +13,21 @@ export class ObjectId {
       throw new Error('Invalid type');
     }
 
+    this.data = Buffer.allocUnsafe(14);
 
-    this.data = Buffer.alloc(14);
 
+    this.data.writeUIntBE(timestamp, 0, 6); // timestamp
 
-    this.data.writeIntLE(type, 1,1); // type 
-    this.data.writeIntLE(timestamp, 0, 6); // timestamp
+    
+    
+    this.data.writeUIntBE(ObjectId.random, 6, 4); //random number 
 
-    const random = Math.floor(Math.random() * 2147483647);
-    this.data.writeIntLE(random, 2, 4); //random number 
+    this.data.writeUIntBE(ObjectId.counter , 10, 3);
 
-    const counter = (Math.floor(Math.random() * 8388607)+1);
-    this.data.writeIntLE(counter , 3, 3);
+    ObjectId.counter++ % 0xffffff;
+
+    this.data.writeUInt8(type, 13); // type 
+
   }
 
   static generate(type?: number): ObjectId {
