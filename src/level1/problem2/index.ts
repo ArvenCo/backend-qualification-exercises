8892242449e2912f4174c044829a804d894bff7c
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2025-01-04 20:54:54 +0800

    soved up to fixed random

--- src/level1/problem2/index.ts
+++ src/level1/problem2/index.ts
@@ -5,6 +5,24 @@ export class ObjectId {
     /**
      * insert your code here
      */
+
+    
+    if (type < 0 || type > 255) {
+      throw new Error('Invalid type');
+    }
+
+
+    this.data = Buffer.alloc(14);
+
+
+    this.data.writeIntLE(type, 1,1); // type 
+    this.data.writeIntLE(timestamp, 0, 6); // timestamp
+
+    const random = Math.floor(Math.random() * 2147483647);
+    this.data.writeIntLE(random, 2, 4); //random number 
+
+    const counter = (Math.floor(Math.random() * 8388607)+1);
+    this.data.writeIntLE(counter , 3, 3);
   }
 
   static generate(type?: number): ObjectId {
