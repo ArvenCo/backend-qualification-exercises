63990ca4a89d9fa138cff68fbcc15bfd21232c32
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2025-01-04 19:15:54 +0800

    serialize and deserialize scalars and built-in object types

--- src/level1/problem1/index.ts
+++ src/level1/problem1/index.ts
@@ -10,8 +10,38 @@ export function serialize(value: Value): unknown {
   /**
    * insert your code here
    */
+
+
+  if (value instanceof Buffer){
+    return {
+      __t: 'Buffer',
+      __v: Array.from(value)
+    }
+  }
+
+  if (value instanceof Date){
+    return {
+      __t: 'Date',
+      __v: value.getTime()
+    }
+  }
   
-  return;
+  if (value instanceof Set){
+    return {
+      __t: 'Set',
+      __v: Array.from(value)
+    }
+  }
+
+  if (value instanceof Map){
+    return {
+      __t: 'Map',
+      __v: Array.from(value)
+    }
+  }
+  
+  
+  return value;
 }
 
 /**
@@ -22,6 +52,26 @@ export function deserialize<T = unknown>(value: unknown): T {
   /**
    * insert your code here
    */
+
+  if (typeof value === 'object' && value !== null){
+    const { __t, __v } = value as { __t: string, __v: unknown };
+    if (__t === 'Buffer'){
+      return Buffer.from(__v as number[]) as unknown as T;
+    }
+
+    if (__t === 'Date'){
+      return new Date(__v as number) as unknown as T;
+    }
+
+    if (__t === 'Set'){
+      return new Set(__v as unknown[]) as unknown as T;
+    }
+
+    if (__t === 'Map'){
+      return new Map(__v as [unknown, unknown][]) as unknown as T;
+    }
+
+  }
   
-  return;
+  return value as unknown as T;
 }
