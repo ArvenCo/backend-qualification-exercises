6327635b4321a490943a6342592f08462f67da9f
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2025-01-05 16:23:48 +0800

    solved

--- src/level2/problem2/index.ts
+++ src/level2/problem2/index.ts
@@ -8,6 +8,19 @@ export function merge(...args: DowntimeLogs[]): DowntimeLogs {
   let set  = new Set();
 
   logs.sort((a, b) => a[0].getTime() - b[0].getTime());
+ 
+  logs.forEach((log, index) => {
+    if  (index === logs.length - 1) {
+      return;
+    }
+    const overlaped = log[1].getTime() > logs[index+1][0].getTime();
+
+    if (overlaped) {
+      logs[index][0] = log[0];
+      logs[index][1] = logs[index+1][1];
+      logs.splice(index+1, 1);
+    }
+  });
 
   logs.filter((dates) => {
     if(set.has(dates)) {
