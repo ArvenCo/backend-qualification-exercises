75f88bd3da3dbc014fef12b3db1d470515d1b928
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2025-01-05 16:08:35 +0800

    solved islands

--- src/level2/problem2/index.ts
+++ src/level2/problem2/index.ts
@@ -4,5 +4,17 @@ export function merge(...args: DowntimeLogs[]): DowntimeLogs {
   /**
    * insert your code here
    */
-  return [];
+  let logs : DowntimeLogs = args.flat(1);
+  let set  = new Set();
+
+  logs.sort((a, b) => a[0].getTime() - b[0].getTime());
+
+  logs.filter((dates) => {
+    if(set.has(dates)) {
+      return false;
+    }
+    set.add(dates);
+    return true;
+  });
+  return logs;
 }
