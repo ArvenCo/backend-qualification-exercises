f658685e694131bc639f9ba44cbfc43f04017918
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2025-01-05 12:47:14 +0800

    solve level2 problem1

--- src/level2/problem1/index.ts
+++ src/level2/problem1/index.ts
@@ -1,11 +1,31 @@
 export class ExecutionCache<TInputs extends Array<unknown>, TOutput> {
-  constructor(private readonly handler: (...args: TInputs) => Promise<TOutput>) {}
+  private readonly cache = new Map<string, Promise<TOutput>>();
+  private readonly ongoing = new Map<string, Promise<TOutput>>();
+
+  constructor(private readonly handler: (...args: TInputs) => Promise<TOutput>  ) {}
   
   async fire(key: string, ...args: TInputs): Promise<TOutput> {
     /**
      * insert your code here
      */
-  
-    return;
+
+    if (this.cache.has(key)) {
+      return await this.handler(...args);
+    }
+
+    if (this.ongoing.has(key)) {
+      return await this.ongoing.get(key);
+    }
+
+    const execution = this.handler(...args).then((result) => {
+      this.cache.set(key, Promise.resolve(result));
+      this.ongoing.delete(key);
+
+      return result;
+    });
+
+    this.ongoing.set(key, execution);
+
+    return await execution;
   }
 }
